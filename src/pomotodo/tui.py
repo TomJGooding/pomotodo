bd7498dacec78860948244f854658ab216f60689
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-02-04 21:36:48 +0000

    feat(tui): add todo list

--- src/pomotodo/tui.py
+++ src/pomotodo/tui.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Input, Static
+from textual.widgets import Footer, Input, Label, ListItem, ListView, Static
 
 
 class TodoForm(Static):
@@ -7,17 +7,29 @@ class TodoForm(Static):
         yield Input(placeholder="Add ToDo")
 
 
+class TodoList(ListView):
+    def __init__(self, todos: list[str] = []) -> None:
+        super().__init__()
+        self._todos: list[str] = todos
+
+    def compose(self) -> ComposeResult:
+        for todo in self._todos:
+            yield ListItem(Label(todo))
+
+
 class PomotodoApp(App):
     BINDINGS = [("q", "quit", "Quit")]
 
     def compose(self) -> ComposeResult:
         yield TodoForm()
+        yield TodoList()
         yield Footer()
 
     def on_mount(self) -> None:
         self.query_one(Input).focus()
 
-    def on_input_submitted(self) -> None:
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        self.query_one(TodoList).append(ListItem(Label(event.value)))
         self.query_one(Input).value = ""
 
 
