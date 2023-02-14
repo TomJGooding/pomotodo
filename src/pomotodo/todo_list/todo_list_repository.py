56d82dd40746ee2991f5c0156380bfb299d56b7b
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-02-14 19:31:06 +0000

    feat(todo list): add abstract repository

--- src/pomotodo/todo_list/todo_list_repository.py
+++ src/pomotodo/todo_list/todo_list_repository.py
@@ -1,9 +1,21 @@
+from abc import ABC, abstractmethod
+
+from pomotodo.todo_list import todo_model
 from pomotodo.todo_list.todo_model import Todo
 
 
-class FakeRepository:
-    def __init__(self, data) -> None:
-        self.data = data
+class AbstractRepository(ABC):
+    @abstractmethod
+    def add(self, todo: todo_model.Todo):
+        raise NotImplementedError
 
-    def list(self):
+
+class FakeRepository(AbstractRepository):
+    def __init__(self, data: list[dict]) -> None:
+        self.data: list[dict] = data
+
+    def add(self, todo) -> None:
+        self.data.append(todo.to_dict())
+
+    def list(self) -> list[Todo]:
         return [Todo.from_dict(i) for i in self.data]
