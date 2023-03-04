55886cc0686676a25ce85c70755cfc972cb960d1
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-03-04 14:47:44 +0000

    test(todo list): add change todo status test

--- tests/todo_list/test_todo_list_services.py
+++ tests/todo_list/test_todo_list_services.py
@@ -2,7 +2,7 @@ import uuid
 
 from pomotodo.todo_list.model import Todo
 from pomotodo.todo_list.repository import FakeRepository
-from pomotodo.todo_list.services import add_todo, get_all_todos
+from pomotodo.todo_list.services import add_todo, change_todo_status, get_all_todos
 from pomotodo.todo_list.unit_of_work import AbstractUnitOfWork
 
 todo_dicts = [
@@ -57,3 +57,18 @@ def test_get_all_todos():
     todos = [Todo.from_dict(i) for i in todo_dicts]
 
     assert get_all_todos(uow) == todos
+
+
+def test_change_todo_status():
+    uow = FakeUnitOfWork()
+    id = uuid.uuid4()
+
+    add_todo(
+        id=id,
+        description="Todo to complete",
+        complete=False,
+        uow=uow,
+    )
+
+    change_todo_status(id=id, uow=uow)
+    assert uow.todos.get(id).complete is True
